a5743bbe1a14a36abc5ca11dedf5b513612eae7f
Allen McCarty <allenmc@fastmail.com>
2021-04-07 16:24:09 -0700

    first upload

---
+++ getname.php
@@ -0,0 +1,17 @@
+<?php
+$name1=$_POST['name'];
+?>
+
+<!DOCTYPE html>
+    <html>
+        <HEAD>
+        <<title>here's my title</title>
+        </head><!-- comment -->
+        
+        <BODY>
+            <h2><?php echo $name1; ?></h2>
+            
+        </body><!-- comment -->
+        
+    </html><!-- comment -->
+    
